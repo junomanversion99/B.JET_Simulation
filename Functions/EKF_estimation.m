c239a6f5c8c511deca03e83301bc8721b57edf77
Junho <iis08164@gmail.com>
2026-05-21 23:38:37 +0900

    B.JET V2.2
    EKF 상태추정 코드 작성

---
+++ Functions/EKF_estimation.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = EKF_estimation(inputArg1,inputArg2)
+%EKF_ESTIMATION 이 함수의 요약 설명 위치
+%   자세한 설명 위치
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
